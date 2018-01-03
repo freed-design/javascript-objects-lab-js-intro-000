29ea4808b166332ec9bca748bed50cd4ccc75d88
Daniel Dunbar <danielqdunbar@gmail.com>
2018-01-03 00:41:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var recipes = new Object();
 var updateObjectWithKeyAndValue = (object, key, value) => {
   return Object.assign( {}, object, { [key]: value })
 }
+
+destructivelyUpdateObjectWithKeyAndValue = (object, key, value) => {
+  object[key] = value;
+  return object;
+}
