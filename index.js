2ed3db00b32d8fb4b1cea505ca8c1b7357731faa
Daniel Dunbar <danielqdunbar@gmail.com>
2018-01-03 00:45:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ destructivelyUpdateObjectWithKeyAndValue = (object, key, value) => {
   object[key] = value;
   return object;
 }
+
+deleteFromObjectByKey = (object, key) => {
+  var newObj
+  newObj = Object.assign({}, object)
+  delete newObj[key]
+  return newObj
+}
